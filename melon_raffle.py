46fc0d3dd34b55d286855c7615eba8f2d4fabff4
Katie Byers <lobsterkatie@gmail.com>
2015-09-30 22:18:39 -0700

    split into three files

--- melon_raffle.py
+++ melon_raffle.py
@@ -1,43 +1,6 @@
+#WHY CAN'T I JUST IMPORT CUSTOMER_INFO?
+from customer_info import organize_customer_data
 from random import choice
-
-
-class Customer(object):
-    """A customer at Ubermelon."""
-
-    def __init__(self, name, email, street, city, zipcode):
-        self.name = name
-        self.email = email
-        self.street = street
-        self.city = city
-        self.zipcode = zipcode
-
-
-def organize_customer_data(customer_file_path):
-    """Read customer file and return list of customer objects.
-
-    Read file at customer_file_path and create a customer object containing
-        customer information.
-
-    """
-
-    customers = []
-
-    customer_file = open(customer_file_path)
-
-    # Process a file like:
-    #
-    #   customer-name | email | street | city | zipcode
-
-    for line in customer_file:
-        customer_data = line.strip().split("|")
-        name, email, street, city, zipcode = customer_data
-
-        new_customer = Customer(name, email, street, city, zipcode)
-        customers.append(new_customer)
-
-    customer_file.close()
-
-    return customers
 
 
 def pick_winner(customers):
